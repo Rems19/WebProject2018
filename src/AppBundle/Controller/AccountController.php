58a29b1cd98b4ee1cf539420bbca95367cc7f1e1
Rémi Labergère <remi.labergere@gmail.com>
2018-01-08 10:07:01 +0100

    Avancement

--- src/AppBundle/Controller/AccountController.php
+++ src/AppBundle/Controller/AccountController.php
@@ -2,10 +2,8 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -20,46 +18,22 @@ class AccountController extends Controller
         {
             return $this->redirect($this->generateUrl('homepage'));
         }
-        return $this->render('security/login.html.twig', [
+        return $this->render(':security:login.html.twig', [
             'error' => $authUtils->getLastAuthenticationError()
         ]);
     }
 
     /**
      * @Route("/account/register", name="register")
-     * @Method("GET")
      */
-    public function showRegister(Request $request)
+    public function showRegister(UserInterface $user = null)
     {
-        $error = $request->query->get('error');
-        return $this->render('form.html.twig', [
-            'title' => 'Registration',
-            'error' => $error,
-            'fields' => [
-                [
-                    'name' => 'username',
-                    'placeholder' => 'Username',
-                    'type' => 'text'
-                ],
-                [
-                    'name' => 'email',
-                    'placeholder' => 'Email',
-                    'type' => 'email'
-                ],
-                [
-                    'name' => 'password',
-                    'placeholder' => 'Password',
-                    'type' => 'password'
-                ],
-                [
-                    'name' => 'password-conf',
-                    'placeholder' => 'Confirm Password',
-                    'type' => 'password'
-                ]
-            ],
-            'submit' => 'Register',
-            'method' => 'post',
-            'action' => '/register'
+        if ($user != null)
+        {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
+        return $this->render(':security:register.html.twig', [
+            'error' => null
         ]);
     }
 }
